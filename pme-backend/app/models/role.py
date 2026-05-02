0ae0aa1b6319a847a2184ab8315accf13c0ad19d
Gerfel Jay <144815217+stultumJay@users.noreply.github.com>
2026-05-02 08:48:20 +0800

    fix: user and role relationship mismatch

--- pme-backend/app/models/role.py
+++ pme-backend/app/models/role.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
 from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import relationship
 import uuid
 from .base import Base
 
@@ -10,4 +10,5 @@ class Role(Base):
     role_id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     role_name = Column(String(50), nullable=False)
 
-    users = relationship("User", back_populates="role")
+    users = relationship("UserAccount", back_populates="role")
+    
